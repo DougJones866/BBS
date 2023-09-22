342c8fb4b919699e127653d0752d1569efffd827
kibroman <you@example.comkibromanx@gmail.com>
2023-09-21 23:02:21 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Routes} from "react-router-dom";
 import Register from "./Register";
 import Home from "./Home";
 
@@ -7,13 +7,15 @@ import "./styles/styles.css";
 export default function App() {
   return (
     <>
-      <Link to="/home">Home</Link>
+      <Link to="/bbs">Home</Link>
       <Link to="/register">Register</Link>
+     
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/bbs" element={<Home />} />
         <Route path="/register" element={<Register />} />
       </Routes>
-      <Home />
+      
+      
     </>
   );
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,13 @@
 import Boards from "./Boards";
 
-
 export default function Home() {
-  return <>
-  <div className="heading">
-          <h1>BBS</h1>
-          <h3>Click on a board to join in!</h3>
-
-        </div>
-  <Boards />
-  </>;
+  return (
+    <>
+      <div className="heading">
+        <h1>BBS</h1>
+        <h3>Click on a board to join in!</h3>
+      </div>
+      <Boards />
+    </>
+  );
 }
